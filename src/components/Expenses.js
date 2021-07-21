b9d9c289d5e071abcb3751130e5dbb6c1f81a0a8
Rabih Melko <rabih.melko@isae.edu.lb>
2021-07-22 00:28:01 +0300

    Separated into Smaller Component

---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+
+
+function Expenses(props){
+    return (
+        <div className="expenses">
+            { props.items.map(expense => {
+                return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+            })}
+        </div>
+    )
+}
+
+export default Expenses;
+
