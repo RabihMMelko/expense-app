cec563c90738ec7b51e5062584ab7ba41ce675ae
Rabih Melko <rabih.melko@isae.edu.lb>
2021-07-22 23:01:19 +0300

    Created Form to Add Expenses without Listener

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,17 +6,20 @@ const ExpenseForm = () => {
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
-            <label>Title</label>
-            <input type="text" placeholder = "Entrer le titre de la dépense"/>
+          <label>Title</label>
+          <input type="text" placeholder="Entrer le titre de la dépense" />
         </div>
         <div className="new-expense__controls">
-            <label>Amount</label>
-            <input type="number" min = '250' step = '250'/>
+          <label>Amount</label>
+          <input type="number" min="250" step="250" />
         </div>
         <div className="new-expense__controls">
-            <label>Date</label>
-            <input type="date" min='2021-01-01'/>
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" />
         </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
