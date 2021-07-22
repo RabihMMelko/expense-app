46d4877868652526b6ce3a584eb2d96e8079fd86
Rabih Melko <rabih.melko@isae.edu.lb>
2021-07-22 22:39:19 +0300

    Messed up with files removal, about to be fixed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
@@ -15,21 +14,3 @@ function ExpenseItem(props) {
 }
 
 export default ExpenseItem;
-=======
-import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
-
-function ExpenseItem(props) {
-  return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}/>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">LBP {props.amount}</div>
-      </div>
-    </div>
-  );
-}
-
-export default ExpenseItem;
->>>>>>> 36b5f7c838ae3663c10674066f47c3eed1f11b11
