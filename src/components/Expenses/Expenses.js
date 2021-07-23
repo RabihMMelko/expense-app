424c6aab30b7791d70c2fee529708ba381650e9b
Rabih Melko <rabih.melko@isae.edu.lb>
2021-07-23 09:27:29 +0300

    Added Year Filter and Passing it to Expenses Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,14 +2,22 @@ import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const changeFilterYearHandler = (enteredYear) => {
+    console.log(enteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem key={expense.id} {...expense} />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter onChangeFilterYear={changeFilterYearHandler} />
+      <Card className="expenses">
+        {props.expenses.map((expense) => (
+          <ExpenseItem key={expense.id} {...expense} />
+        ))}
+      </Card>
+    </div>
   );
 };
 
