106b489c915edad38dec14b18b062d655c9a2080
Rabih Melko <rabih.melko@isae.edu.lb>
2021-07-23 17:42:56 +0300

    Dynamic Add Expense Form Display

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEdit, setEdit] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const formData = {
       ...enteredExpenseData,
       id: "e" + Math.random().toString(),
     };
     props.onAddExpense(formData);
+    setEdit(false);
   };
+
+  const editHandler = () => {
+    setEdit(true);
+  };
+
+  const hideFormHandler = () => {
+    setEdit(false)
+  }
+
+  console.log(isEdit)
 
   return (
     <div className="new-expense">
-      <ExpenseForm
-        //Custom trigger for when anything
-        //in child element happens
-        onSaveExpenseData={saveExpenseDataHandler}
-      />
+      {!isEdit && <button onClick={editHandler}>Add New Expense</button>}
+      {isEdit && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+      onCancel = {hideFormHandler}/>}
     </div>
   );
 };
